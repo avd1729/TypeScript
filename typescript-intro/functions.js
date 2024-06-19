ceb28fb2367288ff1c8207325a855211ec2b26e6
avd <aravind19112003@gmail.com>
2024-06-19 23:36:46 +0530

    functions

---
+++ typescript-intro/functions.js
@@ -0,0 +1,14 @@
+"use strict";
+function none(a, b) {
+    // void - return
+}
+function add(a, b) {
+    // number - return
+    return a + b;
+}
+const subtract = (a, b) => {
+    return a - b;
+};
+const mult = function (a, b) {
+    return a * b;
+};
---
+++ typescript-intro/functions.ts
@@ -0,0 +1,16 @@
+function none(a:number,b:number){
+    // void - return
+}
+
+function add(a:number,b:number){
+    // number - return
+    return a+b;
+}
+
+const subtract = (a:number , b:number) => {
+    return a-b;
+}
+
+const mult = function (a:number , b:number) : number{
+    return a*b;
+}
