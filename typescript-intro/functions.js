915a6165e4b0c3f57f0f85abb62d51a0faff0717
avd <aravind19112003@gmail.com>
2024-06-19 23:44:57 +0530

    Parameter variations

--- typescript-intro/functions.js
+++ typescript-intro/functions.js
@@ -2,13 +2,20 @@
 function none(a, b) {
     // void - return
 }
-function add(a, b) {
-    // number - return
-    return a + b;
-}
 const subtract = (a, b) => {
     return a - b;
 };
 const mult = function (a, b) {
     return a * b;
 };
+function add(a, b, c) {
+    return c ? a + b + c : a + b;
+} // optional params
+function add2(a, b = 10) {
+    return a + b;
+} // default value
+const add3 = (a, b, ...c) => {
+    return a + b + c.reduce((a, b) => a + b, 0);
+};
+let nums = [1, 2, 3];
+console.log(add3(1, 2, ...nums));
--- typescript-intro/functions.ts
+++ typescript-intro/functions.ts
@@ -2,11 +2,6 @@ function none(a:number,b:number){
     // void - return
 }
 
-function add(a:number,b:number){
-    // number - return
-    return a+b;
-}
-
 const subtract = (a:number , b:number) => {
     return a-b;
 }
@@ -14,3 +9,21 @@ const subtract = (a:number , b:number) => {
 const mult = function (a:number , b:number) : number{
     return a*b;
 }
+
+function add(a:number,b:number ,c?: number){
+    return c ? a+b+c : a+b;
+} // optional params
+
+
+function add2(a:number,b=10){
+    return a+b;
+} // default value
+
+const add3 = (a:number , b:number , ...c: number[]) => {
+    return a + b + c.reduce(
+        (a,b) => a+b , 0
+    ); // rest parameter
+}
+
+let nums = [1,2,3];
+console.log(add3(1,2, ...nums)); 
